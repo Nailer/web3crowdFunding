3ddd9a00a90b75acbc0a9e0d2d063e8482edcba1
Sukuna <ajeemmanuel221@gmail.com>
2025-04-29 21:53:27 +0100

    i added the getUserCampaign function

--- Context/CrowdFunding.js
+++ Context/CrowdFunding.js
@@ -18,5 +18,79 @@ export const CrowdFundingProvider = ({ children }) => {
         const { title, description, amount, deadline } = campaign;
         const web3Modal = new Wenb3Modal();
         const connection = await web3Modal.connect();
+        const provider = new ethers.providers.Web3Provider(connection);
+        const signer = provider.getSigner();
+        const contract = fetchContract(signer);
+
+        console.log(currentAccount);
+        try {
+            const transaction = await contract.createCampaign(
+                currentAccount, // owner
+                title, 
+                description, // description
+                ethers.utils.parseUnits(amount, 18),
+                new Date(deadline).getTime()
+            ); 
+
+            await transaction.wait();
+
+            console.log("Contract call success", transaction);
+            
+        } catch (error) {
+            console.log("contract call failure", error);
+            
+        }
+        
+    }
+
+    const getCampaigns = async () => {
+        const provider = new ethers.providers.JsonRpcProvider();
+        const contract = fetchContract(provider);
+
+        const campaigns = await contract.getCampaigns();
+        const parsedCampaigns = campaigns.map((campaign, i) => ({
+            owner: campaign.owner, 
+            title: campaign.title,
+            description: campaign.description,
+            target: ethers.utils.formatEther(campaign.target.toString()),
+            deadline: campaign.deadline.toNumber(),
+            amountCollected: ethers.utils.formatEther(
+                campaign.amountCollected.toString()
+            ),
+            pId: i,
+        }));
+
+        return parsedCampaigns;
+    };
+
+    const getUserCampaigns = async () => {
+        const provider = new ethers.providers.JsonRpcProvider();
+        const contract = fetchContract(provider);
+
+        const allCampaigns = await contract.getCampaigns();
+
+        const accounts = await window.ethereum.request({
+            method: "eth_accounts",
+        })
+        const currentUser = accounts[0];
+
+        const filteredCampaigns = allCampaigns.filter(
+            (campaign) => 
+                campaign.owner === "0xf39Fd6e51aad88F6F4ce6a88827279cffFb92266"
+        );
+
+        const userData = filteredCampaigns.map((campaign, i) => ({
+            owner: campaign.owner,
+            title: campaign.title,
+            description: campaign.description,
+            target: ethers.utils.formatEther(campaign.target.toString()),
+            deadline: campaign.deadline.toNumber(),
+            amountCollected: ethers.utils.formatEther(
+                campaign.amountCollected.toString()
+            ),
+            pId: i
+        }));
+
+        return userData;
     }
 }
