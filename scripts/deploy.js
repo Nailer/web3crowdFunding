ba66e988f379ff0fca31f81d66cc363145f34001
Sukuna <ajeemmanuel221@gmail.com>
2025-07-13 23:37:17 +0100

    added the project contract and campaign creation

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,5 @@
 const hre = require("hardhat");
-//0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
+//0x5FbDB2315678afecb367f032d93F642f64180aa3
 async function main() {
   // We get the contract to deploy
   const CrowdFunding = await hre.ethers.getContractFactory("CrowdFunding");
